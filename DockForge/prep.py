8c81ccc29458ebe3d58163ef29f4e01730f81a5d
maydolina <minayakici53@gmail.com>
2025-07-16 20:27:14 +0300

    simple_prepare_protein & advanced_prepare_protein implemented

--- DockForge/prep.py
+++ DockForge/prep.py
@@ -1,5 +1,8 @@
 import subprocess
+import os
 from openbabel import pybel
+from pdbfixer import PDBFixer
+from openmm.app import PDBFile
 
 def add_hydrogens(input_pdb, output_pdb):
     # load PDB
@@ -12,7 +15,9 @@ def add_hydrogens(input_pdb, output_pdb):
     mol.write("pdb", output_pdb, overwrite=True)
     print(f"Hydrogens added and saved to {output_pdb}")
 
+
 def add_charges(input_pdb, output_pdbqt):
+
     try:
         subprocess.run([
             "obabel",
@@ -21,12 +26,16 @@ def add_charges(input_pdb, output_pdbqt):
             "-h",  # add H
             "--partialcharge", "gasteiger"
         ], check=True)
+
         print(f"Charges assigned and saved to {output_pdbqt}")
+
     except subprocess.CalledProcessError as e:
         print("Error running Open Babel:", e)
 
+
 # add H, remove waters
-def prepare_protein(input_pdb, output_pdbqt):
+def simple_prepare_protein(input_pdb, output_pdbqt):
+
     subprocess.run([
         "obabel",
         "-ipdb", input_pdb,
@@ -34,6 +43,46 @@ def prepare_protein(input_pdb, output_pdbqt):
         "-h",  # add H
         "--delete", "HOH"  # remove water
     ], check=True)
+
     print(f"Protein prepared and saved to {output_pdbqt}")
 
 
+# add H at ph 7, add charges, remove water&ligands, add missing atoms&residudes
+def advanced_prepare_protein(input_pdb, output_pdbqt, ph=7.0):
+
+    output_pdb = "temp_prepared.pdb"  # temporary intermediate
+
+    fixer = PDBFixer(filename=input_pdb)
+
+    print("Finding missing residues...")
+    fixer.findMissingResidues()
+
+    print("Finding missing atoms...")
+    fixer.findMissingAtoms()
+    fixer.addMissingAtoms()
+
+    print("Removing water & ligands...")
+    fixer.removeHeterogens(keepWater=False)
+
+    print(f"Adding hydrogens at pH {ph}...")
+    fixer.addMissingHydrogens(pH=ph)
+
+    print(f"Writing intermediate PDB to {output_pdb}...")
+    with open(output_pdb, 'w') as f:
+        PDBFile.writeFile(fixer.topology, fixer.positions, f)
+
+    print(f"Converting {output_pdb} to PDBQT {output_pdbqt} with charges...")
+    subprocess.run([
+        "obabel",
+        "-ipdb", output_pdb,
+        "-opdbqt", "-O", output_pdbqt,
+        "-h",
+        "--partialcharge", "gasteiger"
+    ], check=True)
+
+    # remove temporary PDB file
+    if os.path.exists(output_pdb):
+        os.remove(output_pdb)
+        print(f"Temporary file {output_pdb} deleted.")
+
+    print(f"Protein prepared and saved to {output_pdbqt}")
--- tests/test_prep.py
+++ tests/test_prep.py
@@ -1,5 +1,9 @@
-from DockForge.prep import add_hydrogens, add_charges
+from DockForge.prep import add_hydrogens, add_charges, simple_prepare_protein, advanced_prepare_protein
 
 #add_hydrogens("/Users/mina/Downloads/7lyj.pdb", "output.pdb")
 
-add_charges("/Users/mina/Downloads/asp.pdb", "output.pdbqt")
+#add_charges("/Users/mina/Downloads/asp.pdb", "output.pdbqt")
+
+#simple_prepare_protein("/Users/mina/Downloads/7lyj.pdb", "output.pdbqt")
+
+advanced_prepare_protein("/Users/mina/Downloads/7lyj.pdb", "output.pdbqt")
